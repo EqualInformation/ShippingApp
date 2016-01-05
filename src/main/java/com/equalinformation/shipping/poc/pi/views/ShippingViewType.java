fece9b3efcb9742038886b4a7d13666e2b5d750e
bpupadhyaya <bpupadhyaya@gmail.com>
2016-01-05 15:22:06 -0800

    Work for View 4, calendar.

--- src/main/java/com/equalinformation/shipping/poc/pi/views/ShippingViewType.java
+++ src/main/java/com/equalinformation/shipping/poc/pi/views/ShippingViewType.java
@@ -3,6 +3,7 @@ package com.equalinformation.shipping.poc.pi.views;
 import com.equalinformation.shipping.poc.pi.views.view1.View1View;
 import com.equalinformation.shipping.poc.pi.views.view2.View2View;
 import com.equalinformation.shipping.poc.pi.views.view3.View3View;
+import com.equalinformation.shipping.poc.pi.views.view4.View4View;
 import com.vaadin.navigator.View;
 import com.vaadin.server.FontAwesome;
 import com.vaadin.server.Resource;
@@ -13,8 +14,8 @@ import com.vaadin.server.Resource;
 public enum ShippingViewType {
     VIEW1("view 1", View1View.class, FontAwesome.HOME, true),
     VIEW2("view 2", View2View.class, FontAwesome.BAR_CHART_O, false),
-    VIEW3("view 3",View3View.class, FontAwesome.TABLE, false);
-//    REPORTS("view4", View4View.class, FontAwesome.FILE_TEXT_O, true), //TODO add later
+    VIEW3("view 3",View3View.class, FontAwesome.TABLE, false),
+    VIEW4("view 4",View4View.class, FontAwesome.FILE_TEXT_O, true);
 //    SCHEDULE("view5", View4View.class, FontAwesome.CALENDAR_O, false); //TODO add later
 
     private final String viewName;
